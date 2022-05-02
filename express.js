a2e33685850f80c164e1d3023bfdb4827cd81a07
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-02 19:23:08 -0300

    ok

--- express.js
+++ express.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4200
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
+
+app.get('/contatos/:id', (req, res) => {
+    const id = req.params.id;
+
+  return res.json({
+       'id': 3,
+      'nome': 'Renato Alves Soares',
+      'email': 'renatoguara2020@gmail.com',
+      'job': 'Analista de Sistemas',
+      'idade': 50
+
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
